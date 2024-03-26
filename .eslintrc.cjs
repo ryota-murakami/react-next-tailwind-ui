093c9192bc6ec80fee09af612631d11cca50745b
ryota-murakami <dojce1048@gmail.com>
2024-03-26 22:10:41 +0900

    init

---
+++ .eslintrc.cjs
@@ -0,0 +1,14 @@
+module.exports = {
+  root: true,
+  env: {},
+  globals: {},
+  extends: ['ts-prefixer', 'next/core-web-vitals'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: ['tsconfig.json'],
+  },
+  plugins: [],
+  rules: {},
+  settings: {},
+  overrides: [],
+}
