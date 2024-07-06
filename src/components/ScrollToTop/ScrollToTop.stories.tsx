9cee16ea6160db876a1bc3a1fa603f98cc97d995
Ryota Murakami <dojce1048@gmail.com>
2024-07-06 19:18:38 +0900

    feat: Add ScrollToTop component with story and update export paths
    - Added ScrollToTop component with functionality to scroll to the top of the page.
    - Created a story for ScrollToTop component in Storybook.

---
+++ src/components/ScrollToTop/ScrollToTop.stories.tsx
@@ -0,0 +1,382 @@
+import type { Meta, StoryObj } from '@storybook/react'
+
+import { ScrollToTop } from './ScrollToTop'
+
+const meta = {
+  title: 'Components/ScrollToTop',
+  component: ScrollToTop,
+  tags: ['autodocs'],
+} as Meta<typeof ScrollToTop>
+
+export default meta
+
+type Story = StoryObj<typeof ScrollToTop>
+
+export const Default: Story = {
+  render: () => (
+    <div className="h-screen w-full mx-auto flex flex-col gap-2">
+      <ScrollToTop />
+      <div className="relative rounded-xl overflow-auto p-8">
+        <div className="flex flex-col gap-8">
+          <div className="">
+            <span className="font-medium text-sm text-slate-500 font-mono mb-3 dark:text-slate-400">
+              list-disc
+            </span>
+            <ul className="list-disc list-inside text-slate-900 dark:text-slate-200">
+              <li className="">
+                Now this is a story all about how, my life got flipped-turned
+                upside down
+              </li>
+              <li className="">
+                And I'd like to take a minute just sit right there
+              </li>
+              <li className="">
+                I'll tell you how I became the prince of a town called Bel-Air
+              </li>
+            </ul>
+          </div>
+          <div className="">
+            <span className="font-medium text-sm text-slate-500 font-mono mb-3 dark:text-slate-400">
+              list-decimal
+            </span>
+            <ul className="list-decimal list-inside text-slate-900 dark:text-slate-200">
+              <li className="">
+                Now this is a story all about how, my life got flipped-turned
+                upside down
+              </li>
+              <li className="">
+                And I'd like to take a minute just sit right there
+              </li>
+              <li className="">
+                I'll tell you how I became the prince of a town called Bel-Air
+              </li>
+            </ul>
+          </div>
+          <div className="">
+            <span className="font-medium text-sm text-slate-500 font-mono mb-3 dark:text-slate-400">
+              list-none
+            </span>
+            <ul className="list-none list-inside text-slate-900 dark:text-slate-200">
+              <li className="">
+                Now this is a story all about how, my life got flipped-turned
+                upside down
+              </li>
+              <li className="">
+                And I'd like to take a minute just sit right there
+              </li>
+              <li className="">
+                I'll tell you how I became the prince of a town called Bel-Air
+              </li>
+            </ul>
+          </div>
+          <div className="">
+            <span className="font-medium text-sm text-slate-500 font-mono mb-3 dark:text-slate-400">
+              list-disc
+            </span>
+            <ul className="list-disc list-inside text-slate-900 dark:text-slate-200">
+              <li className="">
+                Now this is a story all about how, my life got flipped-turned
+                upside down
+              </li>
+              <li className="">
+                And I'd like to take a minute just sit right there
+              </li>
+              <li className="">
+                I'll tell you how I became the prince of a town called Bel-Air
+              </li>
+            </ul>
+          </div>
+          <div className="">
+            <span className="font-medium text-sm text-slate-500 font-mono mb-3 dark:text-slate-400">
+              list-decimal
+            </span>
+            <ul className="list-decimal list-inside text-slate-900 dark:text-slate-200">
+              <li className="">
+                Now this is a story all about how, my life got flipped-turned
+                upside down
+              </li>
+              <li className="">
+                And I'd like to take a minute just sit right there
+              </li>
+              <li className="">
+                I'll tell you how I became the prince of a town called Bel-Air
+              </li>
+            </ul>
+          </div>
+          <div className="">
+            <span className="font-medium text-sm text-slate-500 font-mono mb-3 dark:text-slate-400">
+              list-none
+            </span>
+            <ul className="list-none list-inside text-slate-900 dark:text-slate-200">
+              <li className="">
+                Now this is a story all about how, my life got flipped-turned
+                upside down
+              </li>
+              <li className="">
+                And I'd like to take a minute just sit right there
+              </li>
+              <li className="">
+                I'll tell you how I became the prince of a town called Bel-Air
+              </li>
+            </ul>
+          </div>
+          <div className="">
+            <span className="font-medium text-sm text-slate-500 font-mono mb-3 dark:text-slate-400">
+              list-disc
+            </span>
+            <ul className="list-disc list-inside text-slate-900 dark:text-slate-200">
+              <li className="">
+                Now this is a story all about how, my life got flipped-turned
+                upside down
+              </li>
+              <li className="">
+                And I'd like to take a minute just sit right there
+              </li>
+              <li className="">
+                I'll tell you how I became the prince of a town called Bel-Air
+              </li>
+            </ul>
+          </div>
+          <div className="">
+            <span className="font-medium text-sm text-slate-500 font-mono mb-3 dark:text-slate-400">
+              list-decimal
+            </span>
+            <ul className="list-decimal list-inside text-slate-900 dark:text-slate-200">
+              <li className="">
+                Now this is a story all about how, my life got flipped-turned
+                upside down
+              </li>
+              <li className="">
+                And I'd like to take a minute just sit right there
+              </li>
+              <li className="">
+                I'll tell you how I became the prince of a town called Bel-Air
+              </li>
+            </ul>
+          </div>
+          <div className="">
+            <span className="font-medium text-sm text-slate-500 font-mono mb-3 dark:text-slate-400">
+              list-none
+            </span>
+            <ul className="list-none list-inside text-slate-900 dark:text-slate-200">
+              <li className="">
+                Now this is a story all about how, my life got flipped-turned
+                upside down
+              </li>
+              <li className="">
+                And I'd like to take a minute just sit right there
+              </li>
+              <li className="">
+                I'll tell you how I became the prince of a town called Bel-Air
+              </li>
+            </ul>
+          </div>
+          <div className="">
+            <span className="font-medium text-sm text-slate-500 font-mono mb-3 dark:text-slate-400">
+              list-disc
+            </span>
+            <ul className="list-disc list-inside text-slate-900 dark:text-slate-200">
+              <li className="">
+                Now this is a story all about how, my life got flipped-turned
+                upside down
+              </li>
+              <li className="">
+                And I'd like to take a minute just sit right there
+              </li>
+              <li className="">
+                I'll tell you how I became the prince of a town called Bel-Air
+              </li>
+            </ul>
+          </div>
+          <div className="">
+            <span className="font-medium text-sm text-slate-500 font-mono mb-3 dark:text-slate-400">
+              list-decimal
+            </span>
+            <ul className="list-decimal list-inside text-slate-900 dark:text-slate-200">
+              <li className="">
+                Now this is a story all about how, my life got flipped-turned
+                upside down
+              </li>
+              <li className="">
+                And I'd like to take a minute just sit right there
+              </li>
+              <li className="">
+                I'll tell you how I became the prince of a town called Bel-Air
+              </li>
+            </ul>
+          </div>
+          <div className="">
+            <span className="font-medium text-sm text-slate-500 font-mono mb-3 dark:text-slate-400">
+              list-none
+            </span>
+            <ul className="list-none list-inside text-slate-900 dark:text-slate-200">
+              <li className="">
+                Now this is a story all about how, my life got flipped-turned
+                upside down
+              </li>
+              <li className="">
+                And I'd like to take a minute just sit right there
+              </li>
+              <li className="">
+                I'll tell you how I became the prince of a town called Bel-Air
+              </li>
+            </ul>
+          </div>
+          <div className="">
+            <span className="font-medium text-sm text-slate-500 font-mono mb-3 dark:text-slate-400">
+              list-disc
+            </span>
+            <ul className="list-disc list-inside text-slate-900 dark:text-slate-200">
+              <li className="">
+                Now this is a story all about how, my life got flipped-turned
+                upside down
+              </li>
+              <li className="">
+                And I'd like to take a minute just sit right there
+              </li>
+              <li className="">
+                I'll tell you how I became the prince of a town called Bel-Air
+              </li>
+            </ul>
+          </div>
+          <div className="">
+            <span className="font-medium text-sm text-slate-500 font-mono mb-3 dark:text-slate-400">
+              list-decimal
+            </span>
+            <ul className="list-decimal list-inside text-slate-900 dark:text-slate-200">
+              <li className="">
+                Now this is a story all about how, my life got flipped-turned
+                upside down
+              </li>
+              <li className="">
+                And I'd like to take a minute just sit right there
+              </li>
+              <li className="">
+                I'll tell you how I became the prince of a town called Bel-Air
+              </li>
+            </ul>
+          </div>
+          <div className="">
+            <span className="font-medium text-sm text-slate-500 font-mono mb-3 dark:text-slate-400">
+              list-none
+            </span>
+            <ul className="list-none list-inside text-slate-900 dark:text-slate-200">
+              <li className="">
+                Now this is a story all about how, my life got flipped-turned
+                upside down
+              </li>
+              <li className="">
+                And I'd like to take a minute just sit right there
+              </li>
+              <li className="">
+                I'll tell you how I became the prince of a town called Bel-Air
+              </li>
+            </ul>
+          </div>
+          <div className="">
+            <span className="font-medium text-sm text-slate-500 font-mono mb-3 dark:text-slate-400">
+              list-disc
+            </span>
+            <ul className="list-disc list-inside text-slate-900 dark:text-slate-200">
+              <li className="">
+                Now this is a story all about how, my life got flipped-turned
+                upside down
+              </li>
+              <li className="">
+                And I'd like to take a minute just sit right there
+              </li>
+              <li className="">
+                I'll tell you how I became the prince of a town called Bel-Air
+              </li>
+            </ul>
+          </div>
+          <div className="">
+            <span className="font-medium text-sm text-slate-500 font-mono mb-3 dark:text-slate-400">
+              list-decimal
+            </span>
+            <ul className="list-decimal list-inside text-slate-900 dark:text-slate-200">
+              <li className="">
+                Now this is a story all about how, my life got flipped-turned
+                upside down
+              </li>
+              <li className="">
+                And I'd like to take a minute just sit right there
+              </li>
+              <li className="">
+                I'll tell you how I became the prince of a town called Bel-Air
+              </li>
+            </ul>
+          </div>
+          <div className="">
+            <span className="font-medium text-sm text-slate-500 font-mono mb-3 dark:text-slate-400">
+              list-none
+            </span>
+            <ul className="list-none list-inside text-slate-900 dark:text-slate-200">
+              <li className="">
+                Now this is a story all about how, my life got flipped-turned
+                upside down
+              </li>
+              <li className="">
+                And I'd like to take a minute just sit right there
+              </li>
+              <li className="">
+                I'll tell you how I became the prince of a town called Bel-Air
+              </li>
+            </ul>
+          </div>
+          <div className="">
+            <span className="font-medium text-sm text-slate-500 font-mono mb-3 dark:text-slate-400">
+              list-disc
+            </span>
+            <ul className="list-disc list-inside text-slate-900 dark:text-slate-200">
+              <li className="">
+                Now this is a story all about how, my life got flipped-turned
+                upside down
+              </li>
+              <li className="">
+                And I'd like to take a minute just sit right there
+              </li>
+              <li className="">
+                I'll tell you how I became the prince of a town called Bel-Air
+              </li>
+            </ul>
+          </div>
+          <div className="">
+            <span className="font-medium text-sm text-slate-500 font-mono mb-3 dark:text-slate-400">
+              list-decimal
+            </span>
+            <ul className="list-decimal list-inside text-slate-900 dark:text-slate-200">
+              <li className="">
+                Now this is a story all about how, my life got flipped-turned
+                upside down
+              </li>
+              <li className="">
+                And I'd like to take a minute just sit right there
+              </li>
+              <li className="">
+                I'll tell you how I became the prince of a town called Bel-Air
+              </li>
+            </ul>
+          </div>
+          <div className="">
+            <span className="font-medium text-sm text-slate-500 font-mono mb-3 dark:text-slate-400">
+              list-none
+            </span>
+            <ul className="list-none list-inside text-slate-900 dark:text-slate-200">
+              <li className="">
+                Now this is a story all about how, my life got flipped-turned
+                upside down
+              </li>
+              <li className="">
+                And I'd like to take a minute just sit right there
+              </li>
+              <li className="">
+                I'll tell you how I became the prince of a town called Bel-Air
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  ),
+}
---
+++ src/components/ScrollToTop/ScrollToTop.tsx
@@ -0,0 +1,40 @@
+'use client'
+
+import React, { useState, useEffect } from 'react'
+
+export const ScrollToTop = () => {
+  const [visible, setVisible] = useState(false)
+
+  const toggleVisibility = () => {
+    if (window.scrollY > 200) {
+      setVisible(true)
+    } else {
+      setVisible(false)
+    }
+  }
+
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth',
+    })
+  }
+
+  useEffect(() => {
+    window.addEventListener('scroll', toggleVisibility)
+    return () => {
+      window.removeEventListener('scroll', toggleVisibility)
+    }
+  }, [])
+
+  return (
+    <button
+      type="button"
+      onClick={scrollToTop}
+      className={`rounded-full p-4 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 fixed bottom-5 right-5 ${visible ? 'block' : 'hidden'}`}
+      style={{ zIndex: 99 }}
+    >
+      ↑
+    </button>
+  )
+}
