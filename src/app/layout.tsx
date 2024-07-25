e8402c296c6406667feb18891c51f26523045012
Ryota Murakami <dojce1048@gmail.com>
2024-07-26 07:39:18 +0900

    refactor: Move global CSS to a new directory
    - Updated components.json to reflect the new path for globals.css.
    - Modified import path in layout.tsx to use the new location of globals.css.
    -

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from 'next'
 import { Inter as FontSans } from 'next/font/google'
-import './globals.css'
+import '@/styles/globals.css'
 
 import { TooltipProvider } from '@/components/ui/tooltip'
 import { cn } from '@/lib/utils'
